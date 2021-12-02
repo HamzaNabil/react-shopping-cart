59aff43024640dc9739ed76efa1f4be63cc5664f
HamzaNabil <hamzanabil913@gmail.com>
2021-12-03 01:58:04 +0200

    header comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
+import Header from "./components/Header/Header";
+
 function App() {
   return (
-    <div className="App">
-      App
+    <div className="layout">
+      <Header />
+      <main>
+        Content
+      </main>
+      <footer> 
+        Footer
+      </footer>
     </div>
   );
 }
