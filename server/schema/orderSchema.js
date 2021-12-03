eef0a72adff18dbd3f23d12406c8b6759db85bc1
HamzaNabil <hamzanabil913@gmail.com>
2021-12-04 01:46:11 +0200

    order

---
+++ server/schema/orderSchema.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const orderSchema =  new mongoose.Schema({
+    name: String,
+    email: String,
+}, {timestamps: true});
+
+module.exports = orderSchema
--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const router = require('./routes/routes');
+const productRouter = require('./routes/routes');
+const orderRouter = require('./routes/orderRoutes');
+const runDB = require('./config/db');
 
 const app = express();
 app.use(bodyParser.json());
-app.use('/', router)
+app.use('/', productRouter);
+app.use('/', orderRouter);
 
-const connectionString = 'mongodb://localhost/react-shopping-cart';
-mongoose.connect(connectionString, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-})
-.then(res=> console.log("connection Done"))
+// Run DB
+runDB();
 
 
 app.listen(5001, () => {
