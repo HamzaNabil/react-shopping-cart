3a08244570ba91e5068b293e6de2f01669401c68
HamzaNabil <hamzanabil913@gmail.com>
2021-12-03 02:14:12 +0200

    footer comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 function App() {
@@ -7,9 +8,7 @@ function App() {
       <main>
         Content
       </main>
-      <footer> 
-        Footer
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../../css/Footer/Footer.css"
+
+function Footer() {
+    return (
+        <footer>
+            &copy; CopyWright Hamza Nabil 2021
+        </footer>
+    )
+}
+
+export default Footer
