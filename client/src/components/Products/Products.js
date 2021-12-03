262e8fb36f435346a9b465a2e9a5467b395773f0
HamzaNabil <hamzanabil913@gmail.com>
2021-12-03 05:50:32 +0200

    add animation

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import "../../css/Products/Products.css"
 import ProductModal from './ProductModal';
+import Bounce from 'react-reveal/Bounce'
 
 function Products(props) {
     const [product, setProduct] = useState("");
@@ -14,22 +15,24 @@ function Products(props) {
     }
 
     return (
-        <div className="products-wrapper"> 
-            {props.products.map(product => (
-                <div className="product-item" key={product.id}> 
-                    <a href="#" onClick={() => openModal(product)}>
-                        <img src={product.imageUrl} alt={product.title} />
-                    </a>
-                    <div className="product-desc">
-                        <p>{product.title}</p>
-                        <span>${product.price}</span>
-                    </div>
-                    <button onClick={() => props.addToCart(product)}> Add To Cart </button>
-                </div> 
-            ))} 
+        <Bounce left cascade>
+            <div className="products-wrapper"> 
+                {props.products.map(product => (
+                    <div className="product-item" key={product.id}> 
+                        <a href="#" onClick={() => openModal(product)}>
+                            <img src={product.imageUrl} alt={product.title} />
+                        </a>
+                        <div className="product-desc">
+                            <p>{product.title}</p>
+                            <span>${product.price}</span>
+                        </div>
+                        <button onClick={() => props.addToCart(product)}> Add To Cart </button>
+                    </div> 
+                ))} 
 
-            <ProductModal product={product} closeModal={closeModal} />
-        </div> 
+                <ProductModal product={product} closeModal={closeModal} />
+            </div>
+        </Bounce> 
     )
 }
 
