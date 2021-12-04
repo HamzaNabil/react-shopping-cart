73264113f26ac95db43c663a3a53b467a17d0bbc
HamzaNabil <hamzanabil913@gmail.com>
2021-12-04 02:16:37 +0200

    add order redux

---
+++ client/src/store/reducers/orderReducer.js
@@ -0,0 +1,14 @@
+import { CLEAR_ORDER, CREATE_ORDER } from "../actions/types";
+
+export const orderReducer = (state = {}, action) => {
+    switch(action.type) {
+        case CREATE_ORDER: 
+            return {order: action.data.order};
+
+        case CLEAR_ORDER:
+            return {order: false}
+
+        default:
+            return state
+    }
+}
