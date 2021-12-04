4a55f20180c8731fb1e6f1587b39778f392a3238
HamzaNabil <hamzanabil913@gmail.com>
2021-12-04 02:25:22 +0200

    refactor

--- client/src/words.js
+++ client/src/words.js
@@ -1,5 +1,14 @@
 export const words =  {
     headerTitle: 'Shopping Cart',
     footerTitle: '© CopyRight HamzaNabil @2021',
-    content: 'Content'
+    cartHeader: 'There is products in cart:',
+    cartTitle: 'Title',
+    cartQty: 'qty',
+    cartPrice: 'price',
+    removeBtn: 'Remove',
+    selectProducts: 'select products',
+    total: 'Total',
+    checkout: 'Checkout',
+    name: 'Name',
+    email: 'Email',
 }
