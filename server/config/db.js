77aa5f16a82acb378dcc7c9e4b33299c34f2496b
HamzaNabil <hamzanabil913@gmail.com>
2021-12-04 06:03:46 +0200

    add configration

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectionString = 'mongodb://localhost/react-shopping-cart';
 function runDB() {
-    mongoose.connect(connectionString, {
+    mongoose.connect(process.env.MONGO_URI || connectionString, {
         useNewUrlParser: true, 
         useUnifiedTopology: true
     })
--- server/server.js
+++ server/server.js
@@ -1,20 +1,29 @@
+require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const productRouter = require('./routes/routes');
 const orderRouter = require('./routes/orderRoutes');
 const runDB = require('./config/db');
 
-
 const app = express();
 app.use(bodyParser.json());
-app.use('/', productRouter);
-app.use('/', orderRouter);
 
 // Run DB
 runDB();
 
+app.use('/', productRouter);
+app.use('/', orderRouter);
 
-app.listen(5001, () => {
+
+if(process.env.NODE_ENV === 'production') {
+    app.use('/', express.static('public'));
+
+    app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"))
+} else {
+    app.get('/' , (req,res) => res.send("API Running"))
+}
+
+const PORT = process.env.PORT
+app.listen(PORT || 5001, () => {
     console.log("Running on Port 5001");
 })
