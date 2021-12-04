2819a9f840c4e047dada11ac568a16d1f768dc74
HamzaNabil <hamzanabil913@gmail.com>
2021-12-04 02:58:01 +0200

    add router

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom';
+import "../../css/Navbar/Navbar.css"
+export default function Navbar() {
+    return (
+        <div className='nav'>
+        <ul>
+          <li> <NavLink to="/" >Home</NavLink> </li>  
+          <li> <NavLink to="/orders" >Orders</NavLink> </li>  
+        </ul>  
+      </div> 
+    )
+}
